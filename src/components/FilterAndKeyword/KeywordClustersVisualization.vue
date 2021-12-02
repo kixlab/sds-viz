318579025d6c909e8d522b971031c34ad5a17700
mhamzaerol <mehamerol@gmail.com>
2021-12-03 00:35:06 +0900

    very basic visualization for filter and keyword panel

--- src/components/FilterAndKeyword/KeywordClustersVisualization.vue
+++ src/components/FilterAndKeyword/KeywordClustersVisualization.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-wrap overflow-y-scroll">
-    <div v-for="keywordCluster in keywordClusters" :key="keywordCluster.id" :class="['mx-2 cursor-pointer', interactionState.chosenKeywordClusterId === keywordCluster.id ? 'hover-bg-gray-400 bg-gray-400' : 'hover:bg-gray-300 bg-gray-200']" v-on:click="setChosenKeywordCluster(keywordCluster.id)">
+    <div v-for="keywordCluster in keywordClusters" :key="keywordCluster.id" :class="['mx-2 cursor-pointer', interactionState.chosenKeywordClusterId === keywordCluster.id ? 'border border-gray-500' : '']" :style="`background-color: ${getColor(keywordCluster.id)}`" v-on:click="setChosenKeywordCluster(keywordCluster.id)">
         {{ keywordCluster.topKeyword }}
     </div>
   </div>
@@ -9,6 +9,8 @@
 <script>
 import {useGlobalStore} from "@/stores/globalStoreAgent.js";
 import {computed} from "vue";
+import * as d3 from "d3";
+
 export default {
   name: "KeywordClustersVisualization",
   components: {
@@ -18,11 +20,31 @@ export default {
     const interactionState = computed(() => store.getInteractionState.value);
     const keywordClusters = computed(() => store.getKeywordClusters.value);
     const setInteractionState = store.setInteractionState;
+    
+    const sortedKeywordClusters = computed(() => {
+      const chosenMetric = interactionState.value.chosenMetric;
+      return Object.values(keywordClusters.value).sort((a, b) => {
+        const aAvgMetricVal = a.metricValues[chosenMetric] / a.subtreeSize;
+          const bAvgMetricVal = b.metricValues[chosenMetric] / b.subtreeSize;
+          return window.globalVars.IS_METRIC_GOODNESS_DIRECT[chosenMetric]
+            ? aAvgMetricVal - bAvgMetricVal
+            : bAvgMetricVal - aAvgMetricVal;
+        })
+    });
+
+    const rankingPercentageById = computed(() => {
+      const ranking = {};
+      sortedKeywordClusters.value.forEach((keywordCluster, index) => {
+        ranking[keywordCluster.id] = index / sortedKeywordClusters.value.length;
+      });
+      return ranking;
+    });
 
     return {
       interactionState,
       setInteractionState,
       keywordClusters,
+      rankingPercentageById,
     };
   },
   methods: {
@@ -35,6 +57,10 @@ export default {
         }
         this.setInteractionState(update);
       },
+      getColor(keywordClusterId) {
+        const rankingP = this.rankingPercentageById[keywordClusterId];
+        return d3.interpolateLab("red", "green")(rankingP);
+      }
   },
 };
 </script>
