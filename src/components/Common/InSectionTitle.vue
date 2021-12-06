89ccdf2ac952ca7c38f0d840e03c3df9e944e96d
mhamzaerol <mehamerol@gmail.com>
2021-12-07 01:50:17 +0900

    Add titles for performance metrics and keywords

---
+++ src/components/Common/InSectionTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-lg font-semibold">
+    <slot> </slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "InSectionTitle",
+  components: {},
+};
+</script>
+
+<style>
+</style>
