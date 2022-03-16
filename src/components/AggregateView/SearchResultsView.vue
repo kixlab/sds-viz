b6802f63b608f09a11979cb265a217cbf555204b
Hyunwoo Kim <khw0726@kaist.ac.kr>
2022-03-16 11:06:18 +0900

    fix bug

--- src/components/AggregateView/SearchResultsView.vue
+++ src/components/AggregateView/SearchResultsView.vue
@@ -4,7 +4,7 @@
       <section-title> Search Results comparison</section-title>
     </div>
     <div class="w-full flex-auto flex flex-row flex-nowrap overflow-x-auto">
-      <div v-for="(result, idx) in searchResults" :key="`${result.query}-${idx}`" class="lg:w-1/2 xl:w-1/3 flex-none h-full flex flex-col mx-1 border">
+      <div v-for="(result, idx) in searchResults" :key="`${result.query}-${result.sessionId}`" class="lg:w-1/2 xl:w-1/3 flex-none h-full flex flex-col mx-1 border">
         <div class="flex-none cursor-pointer" @click="highlightSessions(result)">
           Query: {{result.query}} 
           <br> 
@@ -39,7 +39,12 @@ export default {
     const createLog = inject('createLog')
     const searchResults = computed(() => {
       return selectedSessions.value.map(session => {
-        return session.allClickedItems;
+        return session.allClickedItems.map(item => {
+          return {
+            ...item,
+            sessionId: session.id
+          }
+        })
       }).flat().sort((a, b) => b.clickCounts - a.clickCounts);
     })
 
